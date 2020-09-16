9dcb6ad504f71589685c19486902a3660bdc0543
wojciech_wojcik <wojciech_wojcik@ICTIB010AIO5.eu.nordanglia.com>
2020-09-16 15:01:39 +0200

    question3

---
+++ git1/src/git1/Q3.java
@@ -0,0 +1,23 @@
+package git1;
+
+import java.util.Scanner;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the first number.");
+		String text1 = s.nextLine();
+		int number1 = Integer.parseInt(text1);
+		System.out.println("Enter the first number.");
+		String text2 = s.nextLine();
+		int number2 = Integer.parseInt(text2);
+		
+		
+	   System.out.println("The sum of those is " + (number1+number2));
+		
+			
+	}
+
+}
